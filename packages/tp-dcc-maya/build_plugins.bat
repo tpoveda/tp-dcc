209453af8f03e6b01965588aba647ba7d89cfa95
Tomi Poveda <tpovedatd@gmail.com>
2023-04-26 03:37:11 +0200

    Updated Maya build_plugins.bat file

--- packages/tp-dcc-maya/build_plugins.bat
+++ packages/tp-dcc-maya/build_plugins.bat
@@ -1,16 +1,4 @@
-@echo off
-
-if not exist build (mkdir build)
-pushd build
-
-if not exist win64 (mkdir win64)
-
-pushd win64
-echo "==> Creating Plugins win64 project"
-cmake -DCMAKE_CONFIGURATION_TYPES="Debug;Release" -G "Visual Studio 16 2019" "%~dp0"
-echo "==> Creating Plugins win64 project"
-popd
-popd
-
-
-echo "================> Build process completed successfully!"
+mkdir build
+cmake -H. -Bbuild -G "Visual Studio 16 2019"
+cmake --build build/
+cmake --build build/ --con
