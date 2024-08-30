0ed5bc83b9da65ea6e902e3157ebc69d70e1fd51
Tomi Poveda <tpovedatd@gmail.com>
2024-08-30 19:00:08 +0200

    Added functions to retrieve file extensions to DCC interface

--- tp/dcc/abstract/app.py
+++ tp/dcc/abstract/app.py
@@ -1,14 +1,41 @@
 from __future__ import annotations
 
+import os
+import enum
+from typing import Type
 from abc import abstractmethod
 
 from .base import AFnBase
+from ...python.decorators import classproperty
 
 
 class AFnApp(AFnBase):
     """
     Overloads `AFnBase` exposing functions to handle application related behaviours.
     """
+
+    __extensions__: Type[enum.IntEnum] | None = None
+
+    # noinspection PyPep8Naming,PyMethodParameters
+    @classproperty
+    def FileExtensions(cls) -> Type[enum.IntEnum]:
+        """
+        Getter method that returns the file extensions enumerator for this application.
+
+        :return: file extensions enumerator.
+        """
+
+        return cls.__extensions__
+
+    @abstractmethod
+    def extensions(self) -> tuple[enum.IntEnum]:
+        """
+        Returns a list of application file extensions.
+
+        :return: application file extensions.
+        """
+
+        pass
 
     @abstractmethod
     def version(self) -> int | float:
@@ -39,3 +66,24 @@ class AFnApp(AFnBase):
         """
 
         pass
+
+    def is_valid_extension(self, path: str) -> bool:
+        """
+        Returns whether the given path has a valid extension for the application.
+
+        :param path: path to check.
+        :return: True if the path has a valid extension; False otherwise.
+        """
+
+        extension = ""
+        if path and os.path.isfile(path):
+            directory, filename = os.path.split(path)
+            name, extension = os.path.splitext(filename)
+        else:
+            extension = path
+
+        # Compare extension with enumerators
+        extensions = [member.name.lower() for member in self.extensions()]
+        extension = extension.lstrip(".").lower()
+
+        return extension in extensions
--- tp/dcc/maya/app.py
+++ tp/dcc/maya/app.py
@@ -1,14 +1,34 @@
 from __future__ import annotations
 
+import enum
+
 from maya import cmds
 
 from ..abstract.app import AFnApp
+
+
+class FileExtensions(enum.IntEnum):
+    """
+    Overload of IntEnum that contains the file extensions for Maya.
+    """
+
+    mb = 0
+    ma = 1
 
 
 class FnApp(AFnApp):
     """
     Overloads `AFnApp` exposing functions to handle application related behaviours for Maya application.
     """
+
+    def extensions(self) -> tuple[FileExtensions, ...]:
+        """
+        Returns a list of application file extensions.
+
+        :return: application file extensions.
+        """
+
+        return FileExtensions.ma, FileExtensions.mb
 
     def version(self) -> int | float:
         """
