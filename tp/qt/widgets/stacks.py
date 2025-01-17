4be0628ff8296790dd51e5c68d2b0fb60b0658ec
Tomás Poveda <tpovedatd@gmail.com>
2025-01-17 03:48:51 +0100

    Added custom stack with opacity effect Qt widget

---
+++ tp/qt/widgets/stacks.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from Qt.QtWidgets import QWidget, QStackedWidget
+
+from ..mixins import stacked_animation_mixin
+
+
+@stacked_animation_mixin
+class SlidingOpacityStackedWidget(QStackedWidget):
+    """Custom stack widget that activates opacity animation when current stack index changes"""
+
+    def __init__(self, parent: QWidget | None = None):
+        super().__init__(parent)
