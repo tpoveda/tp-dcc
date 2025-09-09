8eb9d508f889d47fd9e9111a5410ad1965cf2bde
Tomás Poveda <tpovedatd@gmail.com>
2025-09-10 01:43:21 +0200

    Add `TreeViewWidget` to `tp.libs.qt.widgets` module exports

--- packages/tp-qt/src/tp/libs/qt/widgets/__init__.py
+++ packages/tp-qt/src/tp/libs/qt/widgets/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from .window import Window
 from .viewmodel.models import TableModel
 from .viewmodel.tableview import TableViewWidget
+from .viewmodel.treeview import TreeViewWidget
 from .viewmodel.data import BaseDataSource, ColumnDataSource
 
 all = [
