c0c0095a1cc7746bcf286c7f9475754721ed4a26
Tomás Poveda <tpovedatd@gmail.com>
2025-09-24 03:10:26 +0200

    Refactor `AdvanceDeletionWidget`: improve row generation with dynamic font handling, introduce checkbox and horizontal box layout for assets, and add debug logging for checkbox state changes.

--- packages/tp-unreal/plugins/tpUnrealProject/Plugins/tpUnreal/Source/tpUnreal/Private/SlateWidgets/AdvanceDeletionWidget.cpp
+++ packages/tp-unreal/plugins/tpUnrealProject/Plugins/tpUnreal/Source/tpUnreal/Private/SlateWidgets/AdvanceDeletionWidget.cpp
@@ -1,7 +1,9 @@
 #include "SlateWidgets/AdvanceDeletionWidget.h"
 
+#include "DebugHelpers.h"
 #include "AssetRegistry/AssetData.h"
 #include "Components/VerticalBox.h"
+#include "Widgets/Input/SCheckBox.h"
 #include "Widgets/Layout/SScrollBox.h"
 #include "Widgets/Text/STextBlock.h"
 #include "Widgets/Views/SListView.h"
@@ -11,7 +13,7 @@ void SAdvanceDeletionTab::Construct(const FArguments& InArgs)
 	bCanSupportFocus = true;
 
 	AssetsData = InArgs._AssetsData;
-	FSlateFontInfo TitleTextFont = FCoreStyle::Get().GetFontStyle(FName("EmbossedText"));
+	FSlateFontInfo TitleTextFont = GetEmbossedTextFont();
 	TitleTextFont.Size = 30.0f;
 	
 	ChildSlot
@@ -53,13 +55,82 @@ void SAdvanceDeletionTab::Construct(const FArguments& InArgs)
 TSharedRef<ITableRow> SAdvanceDeletionTab::OnGenerateRowForList(TSharedPtr<FAssetData> AssetData,
 	const TSharedRef<STableViewBase>& OwnerTable)
 {
+	if (!AssetData.IsValid()) return SNew(STableRow<TSharedPtr<FAssetData>>, OwnerTable);
+
+	const FString DisplayAssetClassName = AssetData->AssetClassPath.GetAssetName().ToString();
 	const FString DisplayAssetName = AssetData->AssetName.ToString();
+
+	FSlateFontInfo AssetClassNameFont = GetEmbossedTextFont();
+	AssetClassNameFont.Size = 10.0f;
+	FSlateFontInfo AssetNameFont = GetEmbossedTextFont();
+	AssetNameFont.Size = 12.5f;
+	
 	TSharedRef<STableRow<TSharedPtr<FAssetData>>> ListViewRowWidget =
 	SNew(STableRow<TSharedPtr<FAssetData>>, OwnerTable)
 	[
-		SNew(STextBlock)
-		.Text(FText::FromString(DisplayAssetName))
+		SNew(SHorizontalBox)
+		+ SHorizontalBox::Slot()
+		.HAlign(HAlign_Left)
+		.VAlign(VAlign_Center)
+		.FillWidth(0.05f)
+		[
+			ConstructCheckBox(AssetData)
+		]
+		+ SHorizontalBox::Slot()
+		.HAlign(HAlign_Center)
+		.VAlign(VAlign_Fill)
+		.FillWidth(0.6f)
+		[
+			ConstructTextForRowWidget(DisplayAssetClassName, AssetClassNameFont)
+		]
+		+ SHorizontalBox::Slot()
+		[
+			ConstructTextForRowWidget(DisplayAssetName, AssetNameFont)
+		]
 	];
 
 	return ListViewRowWidget;
+}
+
+TSharedRef<SCheckBox> SAdvanceDeletionTab::ConstructCheckBox(const TSharedPtr<FAssetData>& AssetData)
+{
+	TSharedRef<SCheckBox> ConstructedCheckBox = SNew(SCheckBox)
+		.Type(ESlateCheckBoxType::CheckBox)
+		.OnCheckStateChanged(this, &SAdvanceDeletionTab::OnCheckBoxStateSateChanged, AssetData)
+		.Visibility(EVisibility::Visible);
+
+	return ConstructedCheckBox;
+}
+
+TSharedRef<STextBlock> SAdvanceDeletionTab::ConstructTextForRowWidget(const FString& TextContent,
+	const FSlateFontInfo& FontToUse)
+{
+	TSharedRef<STextBlock> ConstructedTextBlock = SNew(STextBlock)
+	.Text(FText::FromString(TextContent))
+	.Font(FontToUse)
+	.ColorAndOpacity(FColor::White);
+
+	return ConstructedTextBlock;
+}
+
+FSlateFontInfo SAdvanceDeletionTab::GetEmbossedTextFont() const
+{
+	return FCoreStyle::Get().GetFontStyle(FName("EmbossedText"));
+}
+
+void SAdvanceDeletionTab::OnCheckBoxStateSateChanged(ECheckBoxState NewState, TSharedPtr<FAssetData> AssetData)
+{
+	switch (NewState)
+	{
+	case ECheckBoxState::Unchecked:
+		DebugHelpers::Print(FString::Printf(TEXT("Asset %s has been unchecked"), *AssetData->AssetName.ToString()), FColor::Red);
+		break;
+	case ECheckBoxState::Checked:
+		DebugHelpers::Print(FString::Printf(TEXT("Asset %s has been checked"), *AssetData->AssetName.ToString()), FColor::Green);
+		break;
+	case ECheckBoxState::Undetermined:
+		break;
+	default:
+		break;
+	}
 }
